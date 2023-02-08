4cb13ae5d077054940d8c83fd2d4017e28ad108f
Arindam515 <deyarindam515@gmail.com>
2023-02-08 23:39:33 +0600

    banner and about desing done by Arindam

--- reclair-frontend/src/Pages/Home/Banner/Banner.js
+++ reclair-frontend/src/Pages/Home/Banner/Banner.js
@@ -1,19 +1,31 @@
 import React from 'react';
-
+import slide1 from '../../../images/about/1.jpg';
+import slide2 from '../../../images/about/2.jpg';
+import slide3 from '../../../images/about/3.jpg';
 const Banner = () => {
-    return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
+  return (
+    <div>
+      <h4 className='text-center font-bold text-2xl'>Pioneers of Solar and Renewable Energy</h4>
+<div className="carousel w-full">
+<div id="item1" className="carousel-item w-full">
+<img src={slide1} className="w-full" />
+</div> 
+<div id="item2" className="carousel-item w-full">
+<img src={slide2} className="w-full" />
+</div> 
+<div id="item3" className="carousel-item w-full">
+<img src={slide3} className="w-full" />
+</div> 
+
+</div> 
+<div className="flex justify-center w-full py-2 gap-2">
+<a href="#item1" className="btn btn-xs">1</a> 
+<a href="#item2" className="btn btn-xs">2</a> 
+<a href="#item3" className="btn btn-xs">3</a> 
+
 </div>
-        </div>
-    );
+    </div>
+);
 };
 
 export default Banner;
