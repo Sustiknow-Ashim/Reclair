eba2813c570dd7114372f0bba4bd413a2e5f0f4b
Firoj Alam <firojalamraju85@gmail.com>
2023-02-09 22:41:41 +0600

    contact page design done

--- reclair-frontend/src/Pages/Home/Home.js
+++ reclair-frontend/src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Contact from '../Contact/Contact';
 import SignUp from '../SignUp/SignUp';
 import Banner from './Banner/Banner';
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Contact></Contact>
             <SignUp></SignUp>
         </div>
     );
