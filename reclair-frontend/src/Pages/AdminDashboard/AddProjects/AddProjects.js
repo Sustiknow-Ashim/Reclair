cb16997d2217e871252c5c95e48aaa746bfef054
Firoj Alam <firojalamraju85@gmail.com>
2023-02-22 22:58:18 +0600

    add project component create and design complete

--- reclair-frontend/src/Pages/AdminDashboard/AddProjects/AddProjects.js
+++ reclair-frontend/src/Pages/AdminDashboard/AddProjects/AddProjects.js
@@ -1,9 +1,74 @@
-import React from 'react';
 
+import React from 'react';
+import { toast } from 'react-hot-toast';
 const AddProjects = () => {
+
+      const handleAddToProjects =(event) =>{
+
+
+
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const image = form.picture.value;
+        const organization = form.organization.value;
+        const seller_address = form.seller_address.value;
+        const total_project_cost = form.total_project_cost.value;
+        const percent_of_investment_done = form.percent_of_investment_done.value;
+        const ppa_rate = form.ppa_rate.value;
+        
+        
+        const projects = {
+            image,
+            name,
+            organization,
+            seller_address,
+            total_project_cost,
+            percent_of_investment_done,
+            ppa_rate
+        }
+        fetch('http://localhost:5000/api/project', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(projects)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.acknowledged) {
+                    toast.success('Projects Add confirm')
+                    form.reset();
+                }
+                else {
+                    toast.error(data.message)
+                }
+
+            })
+
+
+    }
+
     return (
         <div>
-            <h2>Comming Soon .........</h2>
+             <div>
+                <h2 className='text-4xl text-center text-orange-500 font-bold my-4'>Add Projects</h2>
+                <div className='flex justify-center items-center'>
+                    <div className='w-96 bg-gray-800 rounded-md p-7'>
+                        <form onSubmit={handleAddToProjects}>
+                        <input  type="text" name='image' placeholder="Photo Url" className="input mb-6 input-md input-bordered w-full max-w-xs" required />
+                            <input type="text" name='name' placeholder="Name" className="input mb-6 input-md input-bordered w-full max-w-xs" required />
+                            <input type="text" name='organization' placeholder="Organization" className="input mb-6 input-md input-bordered w-full max-w-xs"required  />
+                            <input type="text" name='seller_address' placeholder="Seller Address" className="input mb-6 input-md input-bordered w-full max-w-xs"required  />
+                            <input type="text" name='total_project_cost' placeholder="Total Project Cost" className="input mb-6 input-md input-bordered w-full max-w-xs" required />
+                            <input type="text" name='percent_of_investment_done' placeholder="Investment Done" className="input mb-6 input-md input-bordered w-full max-w-xs" required />
+                            <input type="text" name='ppa_rate' placeholder="PPA Rate" className="input mb-6 input-md input-bordered w-full max-w-xs" required />
+                           <input type="submit" className='w-full btn bg-gray-600' value="Submit" />
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- reclair-frontend/src/Pages/Routes/Router.js
+++ reclair-frontend/src/Pages/Routes/Router.js
@@ -3,10 +3,12 @@ import Main from "../../Layout/Main";
 import Contact from "../Contact/Contact";
 import Home from "../Home/Home";
 import Login from "../Login/Login";
-import Project from "../Project/Project";
 import SignUp from "../SignUp/SignUp";
 import ProjectDetails from "../Project/ProjectDetails";
-import Dashboard from "../Dashboard/Dashboard";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import AdminDashboard from "../AdminDashboard/AdminDashboard";
+import AllUsers from "../AdminDashboard/AllUsers/AllUsers";
+import AddProjects from "../AdminDashboard/AddProjects/AddProjects";
 
 
 
@@ -28,10 +30,6 @@ export const router = createBrowserRouter([
             {
                 path:'/login',
                 element:<Login></Login>
-            },
-            {
-                path:'/dashboard',
-                element:<Dashboard></Dashboard>
             },
             {
                 path:'/contact',
